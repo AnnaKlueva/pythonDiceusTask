8121adc8b2907a4d04b021bf2923a939daf08751
Anna Kliuieva <annakliuieva@Annas-MacBook-Pro.local>
2025-05-22 07:56:28 +0200

    Add headless mode

--- test/base_test.py
+++ test/base_test.py
@@ -4,20 +4,41 @@ import pytest
 from selenium.webdriver.support.wait import WebDriverWait
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+import os
 
 class BaseTest:
 
     @pytest.fixture(autouse=True)
     def init_driver(self):
+        is_ci = os.getenv("CI") == "true"
         warnings.simplefilter("ignore", ResourceWarning)
+
         if builtins.driver_name == 'chrome':
-            self.driver = webdriver.Chrome()
+            options = ChromeOptions()
+            if is_ci:
+                options.add_argument("--headless=new")
+                options.add_argument("--no-sandbox")
+                options.add_argument("--disable-dev-shm-usage")
+                options.add_argument(f"--user-data-dir=/tmp/chrome-user-data-{os.getpid()}")
+            self.driver = webdriver.Chrome(options=options)
+
         elif builtins.driver_name == 'firefox':
-            self.driver = webdriver.Firefox()
+            options = FirefoxOptions()
+            if is_ci:
+                options.add_argument("--headless")
+            self.driver = webdriver.Firefox(options=options)
+
         else:
             raise Exception("Incorrect Browser")
 
-        self.driver.maximize_window()
+        if not is_ci:
+            try:
+                self.driver.maximize_window()
+            except Exception:
+                pass
+
         self.wait = WebDriverWait(self.driver, 10)
         yield self.wait, self.driver
 
